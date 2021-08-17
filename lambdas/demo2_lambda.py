d67123d8751da3bc3e303f80ba47989a3ba08687
muj <mo.mujah@gmail.com>
2021-08-17 10:29:31 +0530

    Change 15

--- lambdas/demo2_lambda.py
+++ lambdas/demo2_lambda.py
@@ -2,7 +2,7 @@ import json
 
 
 def lambda_handler(event, context):
-    print("demo_2 test")
+    print("demo_2 test1")
     return {
         'statusCode': 200,
         'body': json.dumps("Demo lambda 2")
